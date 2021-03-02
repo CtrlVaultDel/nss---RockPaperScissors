db3cf998256d48265eaaac93309587a9f9f2b292
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-03-02 10:17:00 -0600

    Basic message and art added

--- Program.cs
+++ Program.cs
@@ -6,7 +6,64 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Variable initialization
+            int PlayerScore = 0;
+            int ComputerScore = 0;
+            Random rand = new Random();
+
+            // Computer's next throw
+            rand.Next(1, 4);
+
+            // Basic scores message
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine($" | Player: {PlayerScore}  |  Computer: {ComputerScore} |");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("What would you like to throw?");
+            Console.WriteLine("1) Rock");
+            Console.WriteLine("2) Paper");
+            Console.WriteLine("3) Scissors");
+
+            // Capture user's throw
+            string Response = Console.ReadLine();
+        }
+
+        // ASCII art for Rock
+        public void Rock()
+        {
+            Console.WriteLine(@"
+                    _______
+                ---'   ____)
+                    (_____)
+                    (_____)
+                    (____)
+                ---._(___)
+            ");
+        }
+
+        // ASCII art for Paper
+        public void Paper()
+        {
+            Console.WriteLine(@"
+                    _______
+                ---'   ____)__
+                        ______)
+                        _______)
+                        _______)
+                ---.__________)
+            ");
+        }
+
+        // ASCII art for Scissors
+        public void Scissors()
+        {
+            Console.WriteLine(@"
+                    _______
+                ---'   ____)__
+                        ______)
+                      __________)
+                    (____)
+                ---.__(___)
+            ");
         }
     }
 }
